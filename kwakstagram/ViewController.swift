7f27cae4f18d9898bb74735b756443acc9a16e95
jkwaky <jkwak@utexas.edu>
2018-10-01 17:31:41 -0500

    Initial Commit

---
+++ kwakstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kwakstagram
+//
+//  Created by Joseph Kwak on 10/1/18.
+//  Copyright © 2018 Joseph Kwak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
